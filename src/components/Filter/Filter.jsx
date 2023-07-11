0e8e2f23bcb110bde5d4ce753a11b739ad55a105
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-07-11 18:47:05 +0300

    Filter style

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
 
+import css from './Filter.module.css';
+
 export const Filter = ({ value, onChange }) => (
-  <label>
-    Find contacts by name
-    <input value={value} onChange={onChange} type="text" name="filter" />
-  </label>
+  <div className={css.filterWrapper}>
+    <label className={css.filterLabel}>
+      Find contacts by name
+      <input
+        className={css.filterInput}
+        value={value}
+        onChange={onChange}
+        type="text"
+        name="filter"
+      />
+    </label>
+  </div>
 );
 
 Filter.propTypes = {
